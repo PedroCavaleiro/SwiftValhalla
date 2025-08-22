30c030cb27de712ea0f0f3b54a457c082c3668e8
Pedro Cavaleiro <p.cavaleiro@icloud.com>
2025-08-22 15:37:57 +0100

    Fixes precision

--- Sources/SwiftValhalla/Extensions/LegResponseExtension.swift
+++ Sources/SwiftValhalla/Extensions/LegResponseExtension.swift
@@ -10,9 +10,20 @@ import MapKit
 
 extension LegResponse {
     
+    /// Fetches the coordinates from a shape allowing a custom precision
+    /// - Parameter precision: The precision of the coordinates defaulting to 1e-6
+    /// - Returns: A array of CLLocationCoordinate2D
+    public func getCoordinates(precision: Double = 1e-6) -> [CLLocationCoordinate2D] {
+        guard let shape = self.shape else { return [] }
+        return decodePolylineShape(shape, precision: precision)
+    }
+    
+    /// Fetches the coordinates from a shape with the default precision
+    /// This is a computed variable instead of a function for improved performance
+    /// - Returns: A array of CLLocationCoordinate2D
     public var coordinates: [CLLocationCoordinate2D] {
         guard let shape = self.shape else { return [] }
-        return decodePolylineShape(shape, precision: 6)
+        return decodePolylineShape(shape, precision: 1e-6)
     }
     
 }
